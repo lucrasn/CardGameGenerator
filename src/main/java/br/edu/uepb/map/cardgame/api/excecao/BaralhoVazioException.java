8c79946208df055c3eb7116df031886f85a2c0c9
livdenner <livia.s.ferreria@gmail.com>
2026-08-15 18:11:06 -0300

    exceções 'implementadas'

--- src/main/java/br/edu/uepb/map/cardgame/api/excecao/BaralhoVazioException.java
+++ src/main/java/br/edu/uepb/map/cardgame/api/excecao/BaralhoVazioException.java
@@ -1,4 +1,29 @@
 package br.edu.uepb.map.cardgame.api.excecao;
 
+/**
+ * Indica que uma compra foi solicitada quando o baralho não possuía cartas.
+ *
+ * <p>A exceção apenas comunica a ausência de cartas. Cabe ao jogo cliente decidir
+ * se deve reconstruir o baralho, encerrar a partida ou adotar outra regra.
+ *
+ * @author Lívia
+ * @since 1.0
+ */
 public final class BaralhoVazioException extends PartidaException {
+
+
+    /** Cria a exceção com a mensagem padrão. */
+    public BaralhoVazioException() {
+        super("Não há cartas disponíveis no baralho.");
+    }
+
+    /**
+     * Cria a exceção com uma mensagem específica.
+     *
+     * @param mensagem descrição da tentativa de compra que falhou
+     */
+    public BaralhoVazioException(String mensagem) {
+        super(mensagem);
+    }
+
 }
--- src/main/java/br/edu/uepb/map/cardgame/api/excecao/PartidaException.java
+++ src/main/java/br/edu/uepb/map/cardgame/api/excecao/PartidaException.java
@@ -1,4 +1,24 @@
 package br.edu.uepb.map.cardgame.api.excecao;
 
-public abstract class PartidaException  extends RuntimeException{
+/**
+ * Exceção-base para falhas relacionadas ao domínio de uma partida.
+ *
+ * <p>As aplicações clientes devem tratar preferencialmente as subclasses
+ * específicas. Este tipo existe para permitir o tratamento conjunto de qualquer
+ * falha de domínio do framework.
+ *
+ * @author Lívia
+ * @since 1.0
+ */
+public abstract class PartidaException extends RuntimeException {
+
+    /**
+     * Cria uma falha de domínio com uma mensagem explicativa.
+     *
+     * @param mensagem descrição da falha
+     */
+    protected PartidaException(String mensagem) {
+        super(mensagem);
+    }
+
 }
