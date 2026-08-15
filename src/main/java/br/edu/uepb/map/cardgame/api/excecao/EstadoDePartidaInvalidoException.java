1f9596302d8e7d407724dba0a2d212d001c3603c
livdenner <livia.s.ferreria@gmail.com>
2026-08-15 18:42:22 -0300

    implementação EstadoDePartidaInvalidoException

--- src/main/java/br/edu/uepb/map/cardgame/api/excecao/EstadoDePartidaInvalidoException.java
+++ src/main/java/br/edu/uepb/map/cardgame/api/excecao/EstadoDePartidaInvalidoException.java
@@ -1,4 +1,27 @@
 package br.edu.uepb.map.cardgame.api.excecao;
 
+/**
+ * Indica que uma operação foi solicitada em uma fase incompatível da partida.
+ *
+ * <p>Essa exceção representa uma violação do ciclo de vida da partida, como tentar
+ * realizar uma jogada antes do início dos turnos ou depois do encerramento.
+ *
+ * @author Lívia
+ * @since 1.0
+ */
 public final class EstadoDePartidaInvalidoException extends PartidaException {
+
+    /** Cria a exceção com a mensagem padrão. */
+    public EstadoDePartidaInvalidoException() {
+        super("O estado atual da partida não permite esta operação.");
+    }
+
+    /**
+     * Cria a exceção com uma mensagem que explica a operação incompatível.
+     *
+     * @param mensagem descrição do motivo pelo qual o estado é inválido
+     */
+    public EstadoDePartidaInvalidoException(String mensagem) {
+        super(mensagem);
+    }
 }
