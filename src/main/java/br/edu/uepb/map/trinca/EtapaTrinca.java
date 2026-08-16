da8360a47df8b5e90b1a9f08c3f60beb4e0fe7fd
lucrasn <lucrasnobrega.araujo@gmail.com>
2026-08-16 14:58:10 -0300

    fix: remove cliente Trinca da main

--- src/main/java/br/edu/uepb/map/trinca/EtapaTrinca.java
+++
@@ -1,8 +0,0 @@
-package br.edu.uepb.map.trinca;
-
-import br.edu.uepb.map.cardgame.api.EtapaDeTurno;
-
-/** Fases de decisão de um turno da Trinca. */
-public enum EtapaTrinca implements EtapaDeTurno {
-    COMPRA, DESCARTE
-}
